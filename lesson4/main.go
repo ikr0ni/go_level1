ffbe54dae3dd15b65718ef6578b0e6f0830863ac
a.anisimov <a.anisimov@yadro.com>
2021-12-03 15:22:40 +0300

    1. Заменеа пересборка слайса на своп 2. В методах Get убраны поинтеры

--- lesson4/main.go
+++ lesson4/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"reflect"
 )
 
 type ArrayToSort struct {
@@ -13,19 +14,19 @@ type ArrayToSort struct {
 func (sort *ArrayToSort) Sort() {
 	sort.sorted = make([]int, len(sort.unsorted))
 	copy(sort.sorted, sort.unsorted)
+	swap := reflect.Swapper(sort.sorted)
 	for i := 1; i < len(sort.sorted); i++ {
 		for j := i; j > 0 && sort.sorted[j-1] > sort.sorted[j]; j-- {
-			temp := append(sort.sorted[:j-1], sort.sorted[j], sort.sorted[j-1])
-			sort.sorted = append(temp, sort.sorted[j+1:]...)
+			swap(j-1, j)
 		}
 	}
 }
 
-func (a *ArrayToSort) GetUnsorted() {
+func (a ArrayToSort) GetUnsorted() {
 	fmt.Println("Не сортированный массив выглядел так:", a.unsorted)
 }
 
-func (a *ArrayToSort) GetSorted() {
+func (a ArrayToSort) GetSorted() {
 	fmt.Println("Сортированный массив выглядит так:", a.sorted)
 }
 
