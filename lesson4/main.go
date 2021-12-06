3648dafa1c72fab1c84a5a8e951860a8f36b6fc4
a.anisimov <a.anisimov@yadro.com>
2021-12-06 15:03:22 +0300

    Убрал рефлексию, хотя код с ней выглядел красивее

--- lesson4/main.go
+++ lesson4/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"io"
-	"reflect"
 )
 
 type ArrayToSort struct {
@@ -14,10 +13,11 @@ type ArrayToSort struct {
 func (sort *ArrayToSort) Sort() {
 	sort.sorted = make([]int, len(sort.unsorted))
 	copy(sort.sorted, sort.unsorted)
-	swap := reflect.Swapper(sort.sorted)
+	//swap := reflect.Swapper(sort.sorted)
 	for i := 1; i < len(sort.sorted); i++ {
 		for j := i; j > 0 && sort.sorted[j-1] > sort.sorted[j]; j-- {
-			swap(j-1, j)
+			//swap(j-1, j)
+			sort.sorted[j-1], sort.sorted[j] = sort.sorted[j], sort.sorted[j-1]
 		}
 	}
 }
